8b658e864bdc3f8ce2a777bab9de14565e222d0b
Tiago Cordeiro <tiago@mulhergorila.com>
2019-11-18 19:58:29 -0300

    Decouple sensitive data and improves documentation.
    - melhora readme
    - exemplos de .env
    - fixture para produtos, categorias e variações
    - atualiza dependencias
    
    This closes #2, closes #7

---
+++ contrib/env_gen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+Django SECRET_KEY generator.
+"""
+from django.utils.crypto import get_random_string
+
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+sk = get_random_string(50, chars)
+
+CONFIG_STRING = f"""# Basic config
+SECRET_KEY={sk}
+DEBUG=True
+ALLOWED_HOSTS=localhost, 127.0.0.1
+DATABASE_URL=sqlite:///db.sqlite3
+
+# Email Configuration
+EMAIL_BACKEND=django.core.mail.backends.console.EmailBackend
+EMAIL_HOST=localhost
+EMAIL_PORT=25
+EMAIL_USE_TLS=False
+EMAIL_HOST_USER=
+EMAIL_HOST_PASSWORD=
+"""
+
+# Writing our configuration file to '.env'
+with open('.env', 'w') as configfile:
+    configfile.write(CONFIG_STRING)
