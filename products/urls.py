3957c8ce41bdb0fdecaf90abbd1431c8a8b5bf0c
Tiago Cordeiro <tiago@mulhergorila.com>
2019-11-21 09:51:36 -0300

    Adiciona ideias para produtos de dois sabores.
    Está relacionado a issue #11

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('list/', views.products_list, name='products-list'),
+    path('list/two-flavors', views.two_flavors, name='two-flavors'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Min
 
 from .models import ProductCategory, ProductVariation, ProductIngredient
 
@@ -14,3 +15,15 @@ def products_list(request):
         'ingredientes': ingredientes,
     }
     return render(request, 'products/list.html', context=context)
+
+
+def two_flavors(request):
+    variacoes = ProductVariation.objects.all()
+    min_price_broto = ProductVariation.objects.filter(variation__name='Broto').aggregate(Min('price'))
+    min_price_grande = ProductVariation.objects.filter(variation__name='Grande').aggregate(Min('price'))
+    context = {
+        'variacoes': variacoes,
+        'min_price_broto': min_price_broto['price__min'],
+        'min_price_grande': min_price_grande['price__min']
+    }
+    return render(request, 'products/sliced.html', context=context)
